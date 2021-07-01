8c5ade69c16a6d1507b8ad69b1a3511fa8e431b4
jauhhari <ervanadiwijaya@gmail.com>
2021-07-01 22:01:55 +0700

    ok

---
+++ tugas27.js
@@ -0,0 +1,17 @@
+function konversi() {
+    var a = "No";
+    var b = " rumah saya";
+    var c = 30;
+    var d = "A";
+
+    var e = "Jalan pangeran Tirtayasa No";
+    var f = 70;
+
+    var kal1 = a + b + " " + String(c) + d;
+    var kal2 = e + " " + String(f);
+
+    console.log(kal1);
+    console.log(kal2);
+}
+
+konversi()
